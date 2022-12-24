fea5337e4535b9f099fe22bace20265b33039acd
daniel-efrat <112973515+daniel-efrat@users.noreply.github.com>
2022-12-24 01:46:13 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -30,8 +30,9 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: start;
   color: #2c3e50;
   margin: 60px;
+  direction:rtl;
 }
 </style>
